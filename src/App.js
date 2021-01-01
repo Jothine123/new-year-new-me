35e619782149619f7e763468c1eb17364fc345ee
Jothine Nandakumar <60521482+Jothine123@users.noreply.github.com>
2021-01-01 15:15:37 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          New Year New Me 
+      <header>
+        <p className="header">New Year New Me</p>
+      </header>
+
+      <nav>
+        <div className="navbar">
+          <ul>
+            <li><a href="home">Home</a></li>
+            <li><a href="about">About Us</a></li>
+            <li><a href="contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
     </div>
   );
 }
