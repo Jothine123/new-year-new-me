a2043551d04cdb5370967b39710a9f718c145fc3
Jothine Nandakumar <60521482+Jothine123@users.noreply.github.com>
2021-01-01 15:31:49 -0500

    Integration of Bootstrap Template
    integrated the bootstrap template into react

--- src/App.js
+++ src/App.js
@@ -2,20 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <p className="header">New Year New Me</p>
-      </header>
-
-      <nav>
-        <div className="navbar">
-          <ul>
-            <li><a href="home">Home</a></li>
-            <li><a href="about">About Us</a></li>
-            <li><a href="contact">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
+    <div className="Apple">
 
     </div>
   );
